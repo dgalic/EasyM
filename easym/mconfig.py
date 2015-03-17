1028f23ea754d750fd864f37e7f51e90c0f73321
galichet <galichet.david@yahoo.fr>
2015-03-17 15:17:28 +0100

    revision to mconfig

--- easym/mconfig.py
+++ easym/mconfig.py
@@ -1,18 +1,31 @@
+#!/usr/bin/env python2.7
+# -*- coding: UTF-8 -*-
 """
-    Le module MConfig perrmet de gérer le fichier de configuration
+    Le module MConfig permet de gérer le fichier de configuration
     incluant les sélectuer css pour chaque site internet possédant
     les informations nécéssaire aux projet.
 """
 
 # imports
-from ConfigParser import Configparser
+from ConfigParser import ConfigParser
+from mfiltre import Mfiltre as mfiltre
+from mconstant import *
 
 # constants
-CONFIG_FILE='configm.cfg'
-# exception classes
-# classes
+CONFIG_FILE     = 'mconfig.cfg'
+URL_TITLE       = 'title'
+URL_CONTENT     = 'content'
+URL_CUT         = 'url_cut'
+TITLE_SELECT    = 'title_select'
+TITLE_FIELD     = 'title_field'
+CONTENT_SELECT  = 'content_allselect'
+CONTENT_FILTRE  = 'content_filtre'
+ALLFIELD_NECESSITY = [
+        URL_TITLE, URL_CONTENT, URL_CUT,
+        TITLE_SELECT,TITLE_FIELD,
+        CONTENT_SELECT, CONTENT_FILTRE]
 
-class Config(object):
+class Mconfig(object):
     """
         La classe s'occupe d'un fichier CONFIG_FILE , se charge de le
         crée s'il n'existe pas, chaque section du fichier correspond à un site.
@@ -26,28 +39,86 @@ class Config(object):
 
     def __init__(self):
         """ On vérifie si le fichier CONFIG_FILE , on le crée sinon"""
-        self.mconfig = ConfigParser.ConfigParser()
-        self.load_source()
+        self.mconfig = ConfigParser()
+        self.loadConfig()
 
-    # Va charger le fichierr CONFIG_FILE dans mconfig
+    # Load file name CONFIG_FILE in yours mconfig
     def loadConfig(self):
-        pass
+        return self.mconfig.read(CONFIG_FILE)
 
-    # ajoute ajoute la nouvelle insatnce new file aux fichier de config
-    def addSource(self, new_file):
-        pass
 
-    # Sauvegarde le fichier de configuration crourant
+    # Save your mconfig in file name CONFIG_FILE
     def saveConfig(self):
+        try:
+           f_mconfig = open(EASY_CONFIG, "wb")
+           self.mconfig.write(f_mconfig)
+           f.close(f_mconfig)
+        except IOError:
+            return False
+        return True
+
+    def isMfiltre(self, name, content):
+        if name in self.mconfig.sections():
+            print 'name is allready exist'
+            return False
+
+        # check if field as necessity to filtre as present
+        field_view = []
+        for elt in content.keys():
+            if elt in ALLFIELD_NECESSITY:
+                field_view.append(elt)
+
+        lfv = len(field_view)
+        laf = len(ALLFIELD_NECESSITY)
+        if lfv != laf:
+            print '%d /%d : all necessity fied not present' % (lfv, laf)
+            return False
+
+        # check list to CONTENT_SELECT have a good format 
+        for elt in content[CONTENT_SELECT]:
+            # elt is empty
+            if not elt:
+                print 'CONTENT_FIELD is empty'
+                return False
+            # check type
+            typ = elt[0]
+            if not typ in [TYPE_ATTRS, TYPE_TEXT, TYPE_TABLE]:
+                print 'Type %s not exist' % (typ)
+                return False
+
+            # check length
+            length = len(elt)
+            field_test = []
+            if length == 3:
+                field = elt[2]
+                if typ == TYPE_TABLE:
+                    for k,v in field.items():
+                        if not v in ALLFIELD_MINFO:
+                            print 'Field %s not exist to MINFO' % (v)
+                            return False
+                else:
+                    if not field in ALLFIELD_MINFO:
+                        print  'field %s not exist to MINFO' % (field)
+                        return False
+            else:
+                print 'Length to elt not equal 3'
+                return False
+
+        return True
+
+    # add a new web site ; name = URL content=dico(option,field)
+    def addSection(self, name, content):
+        if self.testNewSection(name, content):
+            pass
+        else:
+            pass
+
+    def createFiltre(self, name):
+        filtre = mfiltre(name)
+        #Constant url
+        url_title = self.mconfig.get(name, )
+        #Constant title
+        #Constant content
         pass
-
-    # Sort les information d'un section de notre configuration dans un dico
-    def readSection(self, section):
-        pass
-
-# internal functions & classes
-
-def check_config_file():
-    return False
 
 # if __name__ == '__main__':
